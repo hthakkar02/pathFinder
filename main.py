b7349c89bb50fc8fb0a1856bacb4a57b48920519
Harsh Thakkar <thakkarharshemail@gmail.com>
2021-12-28 14:51:46 -0600

    Organized files

--- main.py
+++ main.py
@@ -1,236 +1,56 @@
 import pygame
-import math
-from queue import PriorityQueue
+import a_star
 
 WIDTH = 800
-WINDOW = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption("Path Finder Algorithms")
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 255, 0)
-YELLOW = (255, 255, 0)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-PURPLE = (128, 0, 128)
-ORANGE = (255, 165, 0)
-GREY = (128, 128, 128)
-TURQUOISE = (64, 224, 208)
+def main(WIDTH):
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, WIDTH))
+    pygame.display.set_caption("Path Finder Algorithms")
 
+    light_blue = (137, 207, 240)
+    text_color = (255, 255, 255)
+    button_light = (170, 170, 170)
+    button_dark = (100, 100, 100)
 
-class Node:
-    def __init__(self, row, col, width, total_rows):
-        self.row = row
-        self.col = col
-        self.x = row * width
-        self.y = col * width
-        self.total_rows = total_rows
-        self.color = WHITE
-        self.neighbors = []
-        self.width = width
-
-    def get_position(self):
-        return self.row, self.col
-
-    def is_closed(self):
-        return self.color == RED
-
-    def is_open(self):
-        return self.color == GREEN
-
-    def is_barrier(self):
-        return self.color == BLACK
-
-    def is_start(self):
-        return self.color == ORANGE
-
-    def is_end(self):
-        return self.color == TURQUOISE
-
-    def reset(self):
-        self.color = WHITE
-
-    def make_closed(self):
-        self.color = RED
-
-    def make_open(self):
-        self.color = GREEN
-
-    def make_barrier(self):
-        self.color = BLACK
-
-    def make_start(self):
-        self.color = ORANGE
-
-    def make_end(self):
-        self.color = TURQUOISE
-
-    def make_path(self):
-        self.color = PURPLE
-
-    def draw(self, window):
-        pygame.draw.rect(window, self.color, pygame.Rect(self.x, self.y, self.width, self.width))
-
-    def update_neighbors(self, grid):
-        self.neighbors = []
-        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():  # DOWN
-            self.neighbors.append(grid[self.row + 1][self.col])
-
-        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():  # UP
-            self.neighbors.append(grid[self.row - 1][self.col])
-
-        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():  # RIGHT
-            self.neighbors.append(grid[self.row][self.col + 1])
-
-        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():  # LEFT
-            self.neighbors.append(grid[self.row][self.col - 1])
-
-
-def h(p1, p2):
-    x1, y1 = p1
-    x2, y2 = p2
-    return abs(x2 - x1) + abs(y2 - y1)
-
-
-def make_grid(rows, width):
-    grid = []
-    gap = width // rows
-    for row in range(rows):
-        grid.append([])
-        for column in range(rows):
-            grid[row].append(Node(row, column, gap, rows))
-    return grid
-
-
-def draw_grid(window, rows, width):
-    gap = width // rows
-    for row in range(rows):
-        pygame.draw.line(window, GREY, (0, row * gap), (width, row * gap))
-        for column in range(rows):
-            pygame.draw.line(window, GREY, (column * gap, 0), (column * gap, width))
-
-
-def draw(window, grid, rows, width):
-    window.fill(WHITE)
-    for row in grid:
-        for node in row:
-            node.draw(window)
-    draw_grid(window, rows, width)
-    pygame.display.update()
-
-
-def get_click_pos(mouse_pos, rows, width):
-    gap = width // rows
-    y, x = mouse_pos
-    return y // gap, x // gap
-
-
-def reconstruct_path(came_from, current, draw):
-    while current in came_from:
-        current = came_from[current]
-        current.make_path()
-        draw()
-
-
-def a_star(draw, grid, start, end):
-    count = 0
-    open_set = PriorityQueue()
-    open_set.put((0, count, start))
-    came_from = {}
-    g_score = {spot: float("inf") for row in grid for spot in row}
-    g_score[start] = 0
-    f_score = {spot: float("inf") for row in grid for spot in row}
-    f_score[start] = h(start.get_position(), end.get_position())
-
-    open_set_hash = {start}
-
-    while not open_set.empty():
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-
-        current = open_set.get()[2]
-        open_set_hash.remove(current)
-
-        if current == end:
-            reconstruct_path(came_from, end, draw)
-            end.make_end()
-            return True
-
-        for neighbor in current.neighbors:
-            temp_g_score = g_score[current] + 1
-
-            if temp_g_score < g_score[neighbor]:
-                came_from[neighbor] = current
-                g_score[neighbor] = temp_g_score
-                f_score[neighbor] = temp_g_score + h(neighbor.get_position(), end.get_position())
-                if neighbor not in open_set_hash:
-                    count += 1
-                    open_set.put((f_score[neighbor], count, neighbor))
-                    open_set_hash.add(neighbor)
-                    neighbor.make_open()
-
-        draw()
-
-        if current != start:
-            current.make_closed()
-    return False
-
-
-def main(window, width):
-    rows = 50
-    grid = make_grid(rows, width)
-
-    start = None
-    end = None
+    font = pygame.font.SysFont('Corbel', 35)
+    text = font.render("A Star Algorithm", True, text_color)
+    button1_w = ((text.get_width() // 10) + 5) * 10
+    button1_h = ((text.get_height() // 10) + 2) * 10
 
     run = True
-    started = False
-
+    choice = None
     while run:
-        draw(window, grid, rows, width)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        mouse = pygame.mouse.get_pos()
+        for ev in pygame.event.get():
+
+            if ev.type == pygame.QUIT:
                 run = False
-            if started:
-                continue
-            if pygame.mouse.get_pressed()[0]:  # Left Mouse Click
-                mouse_pos = pygame.mouse.get_pos()
-                row, column = get_click_pos(mouse_pos, rows, width)
-                node = grid[row][column]
 
-                if not start and node != end and not node.is_barrier():
-                    start = node
-                    start.make_start()
-                elif not end and node != start and not node.is_barrier():
-                    end = node
-                    end.make_end()
-                elif node != start and node != end:
-                    node.make_barrier()
+            # checks if a mouse is clicked
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+                if WIDTH / 2 <= mouse[0] <= WIDTH / 2 + button1_w and WIDTH / 2 <= mouse[1] <= WIDTH / 2 + button1_h:
+                    run = False
+                    choice = "a_star"
 
-            elif pygame.mouse.get_pressed()[2]:  # Right Mouse Click
-                mouse_pos = pygame.mouse.get_pos()
-                row, column = get_click_pos(mouse_pos, rows, width)
-                node = grid[row][column]
-                node.reset()
-                if node == start:
-                    start = None
-                elif node == end:
-                    end = None
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and start and end:
-                    for row in grid:
-                        for spot in row:
-                            spot.update_neighbors(grid)
+        # if mouse is hovered on a button it
+        # changes to lighter shade
+        screen.fill(light_blue)
+        if WIDTH / 2 <= mouse[0] <= WIDTH / 2 + button1_w and WIDTH / 2 <= mouse[1] <= WIDTH / 2 + button1_h:
+            pygame.draw.rect(screen, button_light, [WIDTH / 2, WIDTH / 2, button1_w, button1_h])
 
-                    a_star(lambda: draw(window, grid, rows, width), grid, start, end)
+        else:
+            pygame.draw.rect(screen, button_dark, [WIDTH / 2, WIDTH / 2, button1_w, button1_h])
 
-                if event.key == pygame.K_c:
-                    start = None
-                    end = None
-                    grid = make_grid(rows, width)
+        # superimposing the text onto our button
+        screen.blit(text, (WIDTH / 2 + 25, WIDTH / 2 + 8))
+
+        # updates the frames of the game
+        pygame.display.update()
+    if choice == "a_star":
+        a_star.main(screen, WIDTH)
     pygame.quit()
 
 
 if __name__ == '__main__':
-    main(WINDOW, WIDTH)
+    main(WIDTH)
